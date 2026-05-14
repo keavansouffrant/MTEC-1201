0732a42a01e411ad8d51fb92567452ef45aed450
keavansouffrant <57150151+keavansouffrant@users.noreply.github.com>
2026-05-14 12:40:18 -0400

    Add files via upload

---
+++ SS1_KeavanS/sketch.js
@@ -0,0 +1,174 @@
+// Keavan S
+// The Desert
+// The theme of this piece and future arts will be based on the climate changes made in nature
+
+
+
+
+function setup() {
+  createCanvas(1920, 1080);
+}
+
+function draw() {
+  background('#dfde9a');
+
+  strokeWeight(2);
+  fill ('#f60000');
+  circle(1500, 110, 210);
+  strokeWeight(1);
+ 
+  fill('#786105');
+  rect(0, 800, 1920, 1080);
+
+  strokeWeight(4);
+  fill('#2b2a2a');
+  triangle(100, 800, 600, 800, 200, 600);
+  strokeWeight(1); 
+
+  stroke('#000000');
+  line(1650, 850, 30, 850);
+
+  stroke('#000000');
+  line(1650, 950, 30, 950);
+
+  stroke('#000000');
+  line(1650, 900, 30, 900);
+
+  fill('#003609');
+  rect(500, 600, 50, 200, 20);
+
+  fill('#003609');
+  rect(450, 670, 150, 20, 250);
+
+  fill('#003609');
+  rect(850, 650, 50, 200, 20);
+
+  fill('#003609');
+  rect(650, 750, 40, 200, 20);
+
+  fill('#003609');
+  rect(250, 700, 35, 200, 20);
+
+  strokeWeight(3);
+  fill('#3a362b');
+  rect(1200, 550, 450, 300,);
+  strokeWeight(1);
+
+  strokeWeight(3);
+  fill('#552401');
+  triangle(1200, 550, 1450, 350, 1650, 550);
+  strokeWeight(1);
+
+  strokeWeight(6);
+  fill('#ffffff');
+  circle(1260, 650, 100);
+  strokeWeight(1);
+
+  strokeWeight(6);
+  fill('#ffffff');
+  circle(1590, 650, 100);
+  strokeWeight(1);
+
+  strokeWeight(3);
+  fill('#6f3104');
+  rect(1330, 650, 180, 200,);
+  strokeWeight(1);
+
+  fill('#b19100');
+  circle(1490, 760, 20);
+
+  fill('#003609');
+  rect(150, 750, 40, 200, 20);
+
+  fill('#003609');
+  rect(40, 600, 60, 200, 20);
+
+  strokeWeight(1);
+  ellipse(265, 700, 100, 50);
+  strokeWeight(1);
+
+  fill('#003609');
+  rect(350, 700, 40, 200, 20);
+
+  fill('#003609');
+  rect(450, 700, 40, 200, 20);
+
+  fill('#003609');
+  rect(570, 750, 40, 200, 20);
+
+  fill('#003609');
+  rect(720, 750, 40, 200, 20);
+
+  fill('#003609');
+  rect(785, 750, 40, 200, 20);
+
+  fill('#003609');
+  rect(925, 750, 40, 200, 20);
+
+  fill('#003609');
+  rect(995, 700, 40, 200, 20);
+
+  fill('#003609');
+  rect(1055, 650, 40, 200, 20);
+
+  fill('#003609');
+  rect(1120, 700, 40, 200, 20);
+
+  strokeWeight(6);
+  fill('#003609');
+  rect(1220, 775, 80, 120, 50);
+  strokeWeight(1);
+
+  strokeWeight(6);
+  fill('#003609');
+  rect(1550, 775, 80, 120, 50);
+  strokeWeight(1);
+
+  strokeWeight(1);
+  fill('#0099ff');
+  ellipse(1785, 900, 250, 50);
+  strokeWeight(1);
+
+  strokeWeight(2);
+  fill('#f60000');
+  ellipse(1250, 120, 150, 5);
+  strokeWeight(1); 
+
+  strokeWeight(2);
+  fill('#f60000');
+  ellipse(1750, 120, 150, 5);
+  strokeWeight(1);
+
+  strokeWeight(2);
+  fill('#f60000');
+  ellipse(1505, 250, 150, 5);
+  strokeWeight(1);
+
+  
+
+  
+
+  
+  
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+ 
+}
+
+//f60000 red
+//786105 brown
+//000000 black
+//003609 green
+
